cc0d9f53901d90f6741b0c3029b394ed2eff044f
KimHaen-mo <hhg112233445566@gmail.com>
2023-11-09 15:54:38 +0900

    board list

--- myboard/src/main/java/menbershlip/MemberDAO.java
+++ myboard/src/main/java/menbershlip/MemberDAO.java
@@ -9,18 +9,18 @@ public class MemberDAO extends DBConnPool {
 	
 	public MemberDTO getMemberDTO(String uid, String upass) {
 		MemberDTO dto = new MemberDTO(); {
-			String query = "select * from member where id = ? and pass = ?";
+			String query = "select * from member where id = ? and pwd = ?";
 			
 			
 			try{
 				psmt = con.prepareStatement(query);
 				psmt.setString(1, uid);
-				psmt.setString(1, upass);
-				psmt.executeQuery();
+				psmt.setString(2, upass);
+				rs = psmt.executeQuery();
 				
 				if(rs.next()) {
 					dto.setId(rs.getString("id"));
-					dto.setPass(rs.getString("pass"));
+					dto.setPass(rs.getString("pwd"));
 					dto.setName(rs.getString("name"));
 					dto.setRegidate(rs.getString("regidate"));
 				}
