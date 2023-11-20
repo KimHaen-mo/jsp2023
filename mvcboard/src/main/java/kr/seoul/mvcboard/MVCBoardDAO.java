c798a501eae69c6f163282bb4b077fbc9b7758fd
KimHaen-mo <hhg112233445566@gmail.com>
2023-11-20 14:39:34 +0900

    mvcList

--- mvcboard/src/main/java/kr/seoul/mvcboard/MVCBoardDAO.java
+++ mvcboard/src/main/java/kr/seoul/mvcboard/MVCBoardDAO.java
@@ -10,51 +10,40 @@ public class MVCBoardDAO extends DBConnPool {
 	public MVCBoardDAO() {
 		super();
 	}
-	
+
 	public int selectCount(Map<String, Object> map) {
 		int totalCount = 0;
-	    String query = " select coun(*) from mvcboard ";
-	    try {
-	    	stmt = con.createStatement();
-	    	rs = stmt.executeQuery(query);
-	    	rs.next();
-	    	totalCount = rs.getInt(1);
-	    } catch (Exception e) {
-			System.out.println("카운트 중 예외 발생");
+		String query = "select count(*) from mvcboard";
+		if (map.get("searchWord") != null) {
+			query += " where " + map.get("searchField") + " like '%" + map.get("searchWord") + "%'";
+		}
+		try {
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(query);
+			rs.next();
+			totalCount = rs.getInt(1);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 		return totalCount;
 	}
-	
+
 	public List<MVCBoardDTO> selectListPage(Map<String, Object> map) {
-		List<MVCBoardDTO> board = new Vector<MVCBoardDTO>();
-		
-		String query = " "
-					+ " SELECT * FROM ( "
-					+ "     SELECT Tb.*, ROWNUM rNum FRTOM ( "
-					+ "        SELECT * FROM mvcboard";
-		
-		if(map.get("searchWord") != null)
-		{
-			query += " WHERE " + map.get("searchField")
-			      + " LIKE '%" + map.get("searchWord") + "%' ";
+		List<MVCBoardDTO> board = new Vector<>();
+		String query = "select * from mvcboard";
+		if (map.get("searchWord") != null) {
+			query += " where " + map.get("searchField") + " like '%" + map.get("searchWord") + "%'";
 		}
-		
-		query += "       ORDER BY idx DESC"
-				+ "    ) Tb "
-				+ " ) "
-				+ " WHERE rNum BETWEEN ? AND ?";
-		
+		query += " order by idx desc limit ?, ?";
 		try {
 			psmt = con.prepareStatement(query);
-			psmt.setString(1, map.get("start").toString());
-			psmt.setString(2, map.get("end").toString());
+			psmt.setInt(1, Integer.parseInt(map.get("start").toString()));
+			psmt.setInt(2, Integer.parseInt(map.get("end").toString()));
 			rs = psmt.executeQuery();
-			
+
 			while (rs.next()) {
 				MVCBoardDTO dto = new MVCBoardDTO();
-				
+
 				dto.setIdx(rs.getString(1));
 				dto.setName(rs.getString(2));
 				dto.setTitle(rs.getString(3));
@@ -65,14 +54,13 @@ public class MVCBoardDAO extends DBConnPool {
 				dto.setDowncount(rs.getInt(8));
 				dto.setPass(rs.getString(9));
 				dto.setVisitcount(rs.getInt(10));
-				
+
 				board.add(dto);
 			}
-		}
-		catch (Exception e) {
-			System.out.println("게시물 조회 중 예외 발생");
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return board;
 	}
+
 }
